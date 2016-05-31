679a8d62023abee4aa9403726fb9b648020212cc
WithXX168 <chenjinbo_1989@126.com>
2016-05-31 10:23:56 +0800

    git与eclipse结合

---
+++ src/com/cjb/main/HelloWorld.java
@@ -0,0 +1,27 @@
+/**  
+ * ---------------------------------------------------------------------------
+ * Copyright (c) 2016, 深圳市华康全景信息技术有限公司- All Rights Reserved.
+ * Project Name:git_eclipse_demo  
+ * File Name:HelloWorld.java  
+ * Package Name:com.cjb.main
+ * Author   chenjinbo
+ * Date:2016年5月31日上午10:20:37
+ * ---------------------------------------------------------------------------  
+*/  
+  
+package com.cjb.main;  
+/**  
+ * ClassName:HelloWorld 
+ * Date:     2016年5月31日 上午10:20:37
+ * @author   chenjinbo  
+ * @version    
+ * @since    JDK 1.7
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Git Hello World!");
+	}
+
+}
+  
